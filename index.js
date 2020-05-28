f1c64e28e919bf7347bf86b0e3229f41915224c7
Dan Fellner <dfellner92@gmail.com>
2020-05-28 12:26:57 -0400

    new commit

---
+++ index.js
@@ -0,0 +1,121 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Sets up express to server static css/js/images
+app.use(express.static("public"));
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+  
+    app.get("/notes", function(req, res) {
+      res.sendFile(path.join(__dirname, "notes.html"));
+    });
+  
+    // If no matching route is found default to home
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "home.html"));
+    });
+};
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+  
+    app.get("/api/notes", function(req, res) {
+      res.json(tableData);
+    });
+  
+    app.get("/api/waitlist", function(req, res) {
+      res.json(waitListData);
+    });
+  
+    // API POST Requests
+    // Below code handles when a user submits a form and thus submits data to the server.
+    // In each of the below cases, when a user submits form data (a JSON object)
+    // ...the JSON is pushed to the appropriate JavaScript array
+    // (ex. User fills out a reservation request... this data is then sent to the server...
+    // Then the server saves the data to the tableData array)
+    // ---------------------------------------------------------------------------
+  
+    app.post("/api/tables", function(req, res) {
+      // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+      // It will do this by sending out the value "true" have a table
+      // req.body is available since we're using the body parsing middleware
+      if (tableData.length < 5) {
+        tableData.push(req.body);
+        res.json(true);
+      }
+      else {
+        waitListData.push(req.body);
+        res.json(false);
+      }
+    });
+  
+    // ---------------------------------------------------------------------------
+    // I added this below code so you could clear out the table while working with the functionality.
+    // Don"t worry about it!
+  
+    app.post("/api/clear", function(req, res) {
+      // Empty out the arrays of data
+      tableData.length = 0;
+      waitListData.length = 0;
+  
+      res.json({ ok: true });
+    });
+  };
+  
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
